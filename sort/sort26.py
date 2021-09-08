6853324e2acc2b975221726516c753db3246ae41
ksh0414 <89390688+ksh0414@users.noreply.github.com>
2021-09-08 23:55:22 +0900

    Create sort26.py

---
+++ sort/sort26.py
@@ -0,0 +1,18 @@
+from heapq import heappop, heappush
+import sys
+input = sys.stdin.readline
+
+cards = []
+for _ in range(int(input())):
+    heappush(cards, int(input()))
+
+ans = 0
+try:
+    while cards:
+        print(cards)
+        card1 = heappop(cards)
+        card2 = heappop(cards) 
+        ans += card1+card2
+        heappush(cards, card1+card2)
+except IndexError:
+    print(ans)
